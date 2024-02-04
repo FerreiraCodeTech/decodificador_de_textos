7c2a555597545c4dd38bc8c7c73d8400daa0b6bb
Filipe <contatofilipe.sferreira@outlook.com>
2024-02-04 14:58:45 -0300

    Revert "removendo parâmetro deprecated"
    This reverts commit ee936f328e0f8e4f7df4cd2d070162305ffcfe1f.

--- js/validation.js
+++ js/validation.js
@@ -15,7 +15,7 @@ function inputValidation(input) {
 
 function isEmptyValidation(output) {
   if(output.value === '' || output.value === null) {
-    error.createBox(`Impossível copiar uma ${typeof output.value} vazia.`);
+    error.createBox(`Impossível copiar uma ${typeof output.value} vazia.`, 'image4.png');
     return false;
   } else return true;
 }
